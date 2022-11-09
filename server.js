e7d37e0891fe02f18071c55c1fa0d3d2021be72c
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-11-09 15:45:08 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+//Required Middle wares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("Database connection established");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 8080;
+
+const server = app.listen(port, () => {
+  console.log(`Server is running at port: ${port}...`);
+});
